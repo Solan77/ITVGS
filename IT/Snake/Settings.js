0c3293725eeebbd2c15c532ef3668f54844bdc93
Solan77 <Solan77@users.noreply.github.com>
2020-10-21 22:57:36 +0200

    Commit

---
+++ IT/Snake/Settings.js
@@ -0,0 +1,9 @@
+﻿var mycanvas = document.getElenentId('mycanvas');
+var ctx = mycanvas.getContext('2d');
+var snakeSize = 10;
+var w = 350;
+var h = 350;
+var score = 0;
+var snake;
+var snakeSize = 10;
+var food;
